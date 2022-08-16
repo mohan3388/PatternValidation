6509fa54bab469896ff7aab9a7df4262b7591178
mohan <sahumohan338@gmail.com>
2022-08-16 00:01:46 -0700

    [mohan]add: To check users First name validate or not

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Validation validation = new Validation();
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            bool val = validation.FNameValidation(name);
+            if(val)
+            {
+                Console.WriteLine("Validate");
+            }
+            else
+            {
+                Console.WriteLine("Not Validate");
+            }
+        }
+    }
+}
